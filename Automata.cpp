5c6e34498a30994a2e839b0999a153466323a84e
CarlosE2001 <44041684+CarlosE2001@users.noreply.github.com>
2020-11-19 19:20:09 -0600

    Prueba

---
+++ Automata.cpp
@@ -0,0 +1,43 @@
+/*
+TAREA CORTA 2 - CI-0117
+HECHO POR: SEBASTIAN MONTERO CASTRO B95016 & CARLOS ESPINOZA PERAZA B92786
+CONJETURA DE NICOMACO UTILIZANDO HILOS PARALELOS PARA PROCESAR LOS DATOS POR MEDIO DE PARALELIZACION POR DATOS.
+PROFESOR: ALAN CALDERON CASTRO
+*/
+
+#include <iostream>
+#include <omp.h> 
+#include <string>
+using namespace std;
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable : 4996)
+
+bool validaCntHilos(int ch);
+bool validaCntTerminos(long long ct);
+
+int main(int argc, char* argv[]) {
+	int thread_count = 0, n = 0;
+    double tiempo;
+    cout << "--- CONJETURA DE NICOMACO ---" << endl;
+	while (!validaCntHilos(thread_count) || !validaCntTerminos(n)) {
+		cout << "Digite la cantidad de hilos ( >= 1 ): ";
+		cin >> thread_count;
+		cout << "Digite el rango de numeros ( >= 2 ): ";
+		cin >> n;
+    }
+
+    tiempo = omp_get_wtime();
+
+#  pragma omp parallel for num_threads(thread_count)
+	cout << "hola" << endl;
+	return 0;
+}
+
+bool validaCntHilos(int ch) {
+	return ch >= 1;
+}
+
+bool validaCntTerminos(long long ct) {
+	return ct >= 2;
+}
